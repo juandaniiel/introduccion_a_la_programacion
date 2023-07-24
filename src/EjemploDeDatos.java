4cb20eaae4dba832d42b1bbca61405e91eb23a29
JUAN DANIEL <JUAN DANIEL@192.168.1.194>
2023-07-23 21:10:09 -0500

    update2

---
+++ src/EjemploDeDatos.java
@@ -0,0 +1,16 @@
+
+public class EjemploDeDatos {
+
+	public static void main(String[] args) {
+		
+		 int entero = 23;
+		 long enteromaslargo = 456;
+	     double decimales = 12.45;
+	     boolean falsooverdadero = false;
+	     boolean falsooverdadero2 = true;
+	     String texto;
+	     
+	     
+	}
+
+}

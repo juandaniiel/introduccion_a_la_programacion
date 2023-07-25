f777cad5fd0bb52cdf45ef2bc905d501972fedcc
JUAN DANIEL <JUAN DANIEL@192.168.1.194>
2023-07-24 20:04:03 -0500

    update

---
+++ src/Ejercicios_tema_3.java
@@ -0,0 +1,16 @@
+
+public class Ejercicios_tema_3 {
+
+	public static void main(String[] args) {
+		
+		String[] nombres = {"carlos," +" "+ "maria," +" "+  "hugo,"  +" "+ "pablo,"  +" "+ "roberto,"  +" "+ "simon,"  +" "+ "mateo,"  +" "+ "gregorio,"  +" "+ "caterine,"  +" "+ "erlinton"};
+		
+		for(int i = 0; i < nombres.length; i++) {
+			
+			System.out.println(nombres[i] );
+
+		}
+}
+	
+}
+
